6147fab384e614367507d31fbe2670fb1380fffc
Steven Than <steventhan11@gmail.com>
2016-11-12 05:00:11 -0800

    navbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser'
+import { NavbarComponent } from './navbar/navbar.component'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Home';
+  constructor(private titleService: Title) {
+    this.titleService.setTitle(this.title);
+  }
 }
